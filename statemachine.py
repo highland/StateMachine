eca3367d5c463ad178f92bdd4ac54c0242dab4ce
Mark Thomas <mark.highland@gmail.com>
2016-05-22 16:11:36 +0100

    Changes to use typing.NamedTuple, and start_state renamed to initial_state

--- statemachine.py
+++ statemachine.py
@@ -1,5 +1,4 @@
-from collections import namedtuple
-from typing import Iterable, Callable, Dict, Set
+from typing import Iterable, Callable, Dict, Set, NamedTuple
 
 
 class State:
@@ -7,6 +6,7 @@ class State:
     A State (an extended state) captures an aspect of the system's history
     and provides a context for the response of the system to events.
     """
+
     def __init__(self,
                  name: str,
                  entry_action: Callable = None,
@@ -36,30 +36,36 @@ class State:
         return 'State object named {0}.'.format(self.name)
 
 
-Event = str  # type: Event represented by a string
+Event = str  # type alias:  Event is represented by a string
+
+Response = NamedTuple('Response',
+                      [('next_state', State), ('action', Callable[..., None]),
+                       ('guard_condition', Callable[..., bool])])
+"""
+    action may optionally take parameters supplied with the event
+    guard_condition should return False if the transition is to be disabled
+"""
 
 
-Response = namedtuple('Response', 'next_state, action, guard_condition')
-
-
-# noinspection PyCallingNonCallable,PyCallingNonCallable,PyCallingNonCallable
+# noinspection PyCallingNonCallable
 class StateMachine(State):
     """
     An implementation of a FSM (Finite State Machine).
     """
+
     def __init__(self,
                  name: str,
                  event_source: Iterable[Event],
-                 start_state: State = State('start'),
+                 initial_state: State = State('initial'),
                  initial_action: Callable = None,
                  end_action: Callable = None
                  ):
         super().__init__(name)
         self._event_source = event_source
-        self._current_state = start_state
+        self._current_state = initial_state
         self._initial_action = initial_action
         self._end_action = end_action
-        self._states = {start_state}    # type: Set[State]
+        self._states = {initial_state}  # type: Set[State]
         self._state_table = {}  # type: Dict[Event, Dict[State, Response]]
         self._machine_data = {}  # data used by actions and event ignore functions
 
@@ -68,7 +74,7 @@ class StateMachine(State):
 
     def add_event(self, event: Event):
         if event not in self._state_table:
-            self._state_table[event] = {}   # Dict[State, Response]
+            self._state_table[event] = {}  # Dict[State, Response]
 
     def add_table_entry(self, event: Event, state: State, response: Response):
         self.add_event(event)
@@ -82,7 +88,7 @@ class StateMachine(State):
             required_response = self._state_table.get(event).get(self._current_state)
             if required_response:
                 next_state, action, guard_condition = required_response
-                if guard_condition and guard_condition():  # guard_condition function supplied which returns True
+                if guard_condition and not guard_condition():  # guard_condition function supplied which returns False
                     continue
                 self._current_state.do_exit()
                 self._current_state = next_state
